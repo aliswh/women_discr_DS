41c48640f941d66e7d3fcd9e24425a97e177e27a
Alice S <39992505+aliswh@users.noreply.github.com>
2020-11-15 18:51:44 +0100

    factors

--- main.R
+++ main.R
@@ -6,18 +6,29 @@ list_col = c("RIP","PADRENATO","MADRENATO","CARICOLAV","UOMOPRIMA",
              "UOMOSOLDI","UOMODECIDE","NODONNASUP","FAMOSTACOLO",
              "PIUDONNE","DIVISIONEFAM","INADATTI","PIUCURA","D_DISCRIM",
              "D_DISCRIM5AA","RINUNCLAV","RINUNCCAMBIO","RINUNINC",
+             
              "S_DISCRIM","S_SESSO","S_ASPET","C_DISCRIM","C_RETRINF",
              "C_GRAVID","C_SESSO","C_ASPET","L_DISCRIM","L_LICENZ",
              "L_LICGRAV","L_QDLICGRAV","L_LICENZ2","L_LICGRAV2",
              "L_QDLICGRAV2","L_CAPAC2","L_RETRINF2","L_SESSO","L_ASPET",
+             
              "UMILIATA","MIN","RELAZOUT","CUCINA","PULIZIA",
              "CURAFIGLI","DIVISLAV","PIUGUAD","S_PERCOSS",
              "SESSO","ETA","ISTR","CONDIZ"
              )
 
-listone <- c("RIP","PADRENATO","MADRENATO",
-             "CARICOLAV","UOMOPRIMA")
-
 # select cols from dataframe
 df = data.frame(DF_DISCRIM_A2011[list_col])
+
+# get Fact values per selected col
+getFact <- function(list_col) {
+  getFact_list <- c()
+  for (name in list_col) {
+    getFact_list = append(getFact_list, paste(name,'_Fact',sep=''))
+  }
+  return(getFact_list)
+}
+fact_list = getFact(list_col)
+
+pie(table(RIP_Fact))
 
